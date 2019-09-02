706c5c129535682a44dd16c993ca30bb9cca86f7
Ankita Khurana <Ankita.Khurana@netent.com>
2019-09-02 11:56:19 +0530

    Repo selection check

--- utils/rename.js
+++ utils/rename.js
@@ -2,54 +2,81 @@
 var convert = require('xml-js');
 var fs = require("fs");
 
-const[,,...args] = process.argv
+const [, , ...args] = process.argv
 const gameNameWithoutDash = args[0];
 const gameNamewithDash = args[1];
 const gameNameArgs = args.slice(2);
 let gameName = "";
 gameNameArgs.forEach(arg => {
-    gameName+= arg+" ";
+  gameName += arg + " ";
 });
 
-function updatePom(){
 
-    fs.readFile("./pom.xml", function(error, data) {
-      var json = JSON.parse((convert.xml2json(data, {compact: true, spaces: 4}))),
-          xml = data;
+function updateClientPom() {
+  fs.readFile("./pom.xml", function (error, data) {
+    var json = JSON.parse((convert.xml2json(data, { compact: true, spaces: 4 }))),
+      xml = data;
 
-      // Update project Entried    
-      json.project.groupId = "com.netent.casino-software.games.videoslots." + gameNamewithDash;
-      json.project.artifactId = gameNameWithoutDash+"_mobile_html";
-      json.project.name = gameName;
+    // Update project Entried    
+    json.project.groupId = "com.netent.casino-software.games.videoslots." + gameNamewithDash;
+    json.project.artifactId = gameNameWithoutDash + "_mobile_html";
+    json.project.name = gameName;
 
-      // Update Properties 
-      json.project.properties["game-name"] = gameName;
-      json.project.properties["langlib.groupId"] = "com.netent.casino-software.games.videoslots."+gameNamewithDash;
-      json.project.properties["gamerules.groupId"] = "com.netent.casino-software.games.videoslots."+gameNamewithDash;
-      json.project.properties["langlib.artifactId"] = gameNameWithoutDash+"-client-langlib";
-      json.project.properties["gamerules.artifactId"] = gameNameWithoutDash+"-client-gamerules";
+    // Update Properties 
+    json.project.properties["game-name"] = gameName;
+    json.project.properties["langlib.groupId"] = "com.netent.casino-software.games.videoslots." + gameNamewithDash;
+    json.project.properties["gamerules.groupId"] = "com.netent.casino-software.games.videoslots." + gameNamewithDash;
+    json.project.properties["langlib.artifactId"] = gameNameWithoutDash + "-client-langlib";
+    json.project.properties["gamerules.artifactId"] = gameNameWithoutDash + "-client-gamerules";
 
-      // Update XML
-      xml = convert.json2xml(json,{compact: true, ignoreComment: true, spaces: 4})
-      fs.writeFile("./pom.xml", xml, function (err) {
-        if (err) return console.log(err);
-     });
-   });
-
+    // Update XML
+    xml = convert.json2xml(json, { compact: true, ignoreComment: true, spaces: 4 })
+    fs.writeFile("./pom.xml", xml, function (err) {
+      if (err) return console.log(err);
+    });
+  });
 }
 
-function updateJenkins(){
-
-}
-
-
-function updateSonarProperties(){
-
+function updateClient(){
+    updateClientPom();
 }
 
 
+function updateFiles()
+{  
+  var currentDirectory = process.cwd();
+  if (currentDirectory.indexOf('/') !== -1)
+    currentDirectory = currentDirectory.substring(currentDirectory.lastIndexOf('/') + 1);
+  else
+    currentDirectory = currentDirectory.substring(currentDirectory.lastIndexOf('\\') + 1);
 
-// Update project files
-updatePom();
-updateJenkins();
-updateSonarProperties();
+  switch (currentDirectory) {
+    case gameNamewithDash + '-server': updateServer();
+      break;
+
+    case gameNamewithDash + '-configuration-desktop': updateConfigDesktop();
+      break;
+
+    case gameNamewithDash + '-configuration-mobile': updateConfigMobile();
+      break;
+
+    case gameNamewithDash + '-client-generic': updateClient();
+      break;
+
+    case gameNamewithDash + '-distribution': updateDistribution();
+      break;
+
+    case gameNamewithDash + '-gamerules': updateGamerulesPom();
+      break;
+
+    case gameNamewithDash + '-langlib': updateLanglib();
+      break;
+
+    default: break;
+
+  }
+
+}
+
+
+updateFiles();
