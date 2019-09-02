6541af1c295121e655829d1c8e457c26f7448f62
Ankita Khurana <Ankita.Khurana@netent.com>
2019-09-02 14:41:32 +0530

    Gamerules POM

--- utils/rename.js
+++ utils/rename.js
@@ -6,6 +6,7 @@ const [, , ...args] = process.argv
 const gameNameWithoutDash = args[0];
 const gameNamewithDash = args[1];
 const gameNameArgs = args.slice(2);
+const Version = "1.0.0-SNAPSHOT";
 let gameName = "";
 gameNameArgs.forEach(arg => {
   gameName += arg + " ";
@@ -37,10 +38,63 @@ function updateClientPom() {
   });
 }
 
+function updateGameRulesPom() {
+
+  fs.readFile("./pom.xml", function (error, data) {
+    var json = JSON.parse((convert.xml2json(data, { compact: true, spaces: 4 }))),
+      xml = data;
+
+    // Update project Entried    
+    json.project.groupId = "com.netent.casino-software.games.videoslots." + gameNamewithDash;
+    json.project.artifactId = gameNameWithoutDash + "-gamerules";
+
+    // Update Properties 
+    json.project.properties["im-2-git-link"] = gameNamewithDash;
+    json.project.properties["langlib.version"] = Version;
+
+    // update langlib Build
+    for (i = 0; i < json.project.build.plugins.plugin.length; i++) {
+      if (json.project.build.plugins.plugin[i].artifactId === "maven-dependency-plugin") {
+        for (j = 0; i < json.project.build.plugins.plugin[i].executions.execution.length; i++) {
+          if (json.project.build.plugins.plugin[i].executions.execution[j] === "unpack-langlib") {
+            json.project.build.plugins.plugin[i].executions.execution[j].configuration.artifactItems.artifactItem.groupId = "com.netent.casino-software.games.videoslots." + gameNamewithDash;
+            json.project.build.plugins.plugin[i].executions.execution[j].configuration.artifactItems.artifactItem.artifactId = gameNameWithoutDash + "-langlib";
+          }
+        }
+      }
+    }
+
+    // Update XML
+    xml = convert.json2xml(json, { compact: true, ignoreComment: true, spaces: 4 })
+    fs.writeFile("./pom.xml", xml, function (err) {
+      if (err) return console.log(err);
+    });
+  });
+}
+
 function updateClient() {
   updateClientPom();
 }
 
+function updateConfigDesktop() {
+
+}
+
+function updateConfigMobile() {
+
+}
+
+function updateDistribution() {
+
+}
+
+function updateGamerules() {
+  updateGameRulesPom();
+}
+
+function updateLanglib() {
+
+}
 
 function updateFiles() {
   var currentDirectory = process.cwd(), reponame = "";
@@ -66,7 +120,7 @@ function updateFiles() {
     case 'distribution': updateDistribution();
       break;
 
-    case 'gamerules': updateGamerulesPom();
+    case 'gamerules': updateGamerules();
       break;
 
     case 'langlib': updateLanglib();
